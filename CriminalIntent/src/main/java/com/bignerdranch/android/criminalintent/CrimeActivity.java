7d57cbebc388ae8a8bffec3467c219a0889433b9
kyfast <kylie.stradley@gmail.com>
2014-05-13 14:04:36 -0400

    Adds listview of each crime
    * refactor activity_crime to be used by more than one view
    * I just realized this app is a pun because we're using
      intents

--- CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -6,20 +6,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends FragmentActivity {
-    /** Called when the activity is first created. */
+public class CrimeActivity extends SingleFragmentActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-        FragmentManager manager = getSupportFragmentManager();
-        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
-
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            manager.beginTransaction()
-                    .add(R.id.fragmentContainer, fragment)
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
