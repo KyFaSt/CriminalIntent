12e87061dae6c37b4908d0836414831128e9c99f
kyfast <kylie.stradley@gmail.com>
2014-05-13 11:46:28 -0400

    initialize app

---
+++ CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,45 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date Date) {
+        mDate = Date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean Solved) {
+        mSolved = Solved;
+    }
+
+    public Crime() {
+
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+}
